4aef65c28f02ed627969442ac85b39aa4053a524
Vittostore Oraculo <livioponce@vittostore.stoe>
2026-05-05 19:28:28 -0400

    fix: set PUPPETEER_EXECUTABLE_PATH in server.ts for Render

--- server.ts
+++ server.ts
@@ -1,3 +1,5 @@
+import puppeteer from 'puppeteer';
+process.env.PUPPETEER_EXECUTABLE_PATH = puppeteer.executablePath();
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
