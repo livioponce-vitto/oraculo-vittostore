aa651d32f7c4c10d60a2dc3c2e28380072e6b51e
Vittostore Oraculo <livioponce@vittostore.stoe>
2026-05-05 20:23:21 -0400

    fix: pasar executablePath y args recomendados a whatsapp-web.js

--- whatsapp.ts
+++ whatsapp.ts
@@ -1,12 +1,20 @@
 import puppeteer from 'puppeteer';
-process.env.PUPPETEER_EXECUTABLE_PATH = puppeteer.executablePath();
 import { Client, LocalAuth } from 'whatsapp-web.js';
 import qrcode from 'qrcode-terminal';
+
+const executablePath = puppeteer.executablePath();
 const whatsappClient = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        executablePath: puppeteer.executablePath(),
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        executablePath,
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu',
+            '--single-process'
+        ]
     }
 });
 
